9f026cca773b004461f99e2aa53700d288ebc6ad
Peterson Munuera <peterson.munuera@gmail.com>
2021-09-17 19:52:15 -0300

    adicionado miragejs pra criação de api fake

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,14 +1,29 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+
   return(
     <Container>
       <table>
         <thead>
-          <th>Título</th>
-          <th>Preço</th>
-          <th>Categoria</th>
-          <th>Data</th>
+          <tr>
+            <th>Título</th>
+          </tr>
+          <tr>
+            <th>Preço</th>
+          </tr>
+          <tr>
+            <th>Categoria</th>
+          </tr>
+          <tr>
+            <th>Data</th>
+          </tr>
         </thead>
 
         <tbody>
